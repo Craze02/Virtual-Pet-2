f9cc335ef829917e9df3d68133de3c7df01994ae
Craze02 <70636961+Craze02@users.noreply.github.com>
2020-12-21 15:41:54 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,40 @@
+class Food {
+    constructor() {
+        this.foodStock = 0;
+        this.lastFed;
+        this.milkImg = loadImage("images/Milk.png");
+    }
+    getFoodStock() {
+        return this.foodStock;
+    }
+    updateFoodStock(foodStock) {
+        this.foodStock = foodStock;
+    }
+    deductFood() {
+        if (this.foodStock > 0) {
+            this.foodStock = this.foodStock - 1
+        }
+    }
+    getFedTime(lastFed) {
+        this.lastFed = lastFed;
+    }
+    display() {
+        var x = 80;
+        var y = 100;
+        imageMode(CENTER);
+        image(this.milkImg, 720, 220, 70, 70);
+
+        if (this.foodStock !== 0) {
+            for (var i = 0; i < this.foodStock; i++) {
+                if (i % 10 == 0) {
+                    x = 80;
+                    y = y + 50;
+                }
+                image(this.milkImg, x, y, 50, 50);
+                x = x + 30;
+            }
+        }
+    }
+
+}
+
